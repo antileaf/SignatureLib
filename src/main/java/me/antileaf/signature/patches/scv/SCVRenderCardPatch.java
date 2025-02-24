2dff617847370246a29cdaba77c8b8e10b92f9e6
antileaf <antileaf@163.com>
2025-02-24 10:33:12 +0800

    qwq

---
+++ src/main/java/me/antileaf/signature/patches/scv/SCVRenderCardPatch.java
@@ -0,0 +1,266 @@
+package me.antileaf.signature.patches.scv;
+
+import basemod.ReflectionHacks;
+import basemod.patches.com.megacrit.cardcrawl.cards.AbstractCard.MultiCardPreview;
+import basemod.patches.com.megacrit.cardcrawl.cards.AbstractCard.MultiCardPreviewRenderer;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.status.Burn;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.screens.SingleCardViewPopup;
+import javassist.CannotCompileException;
+import javassist.CtBehavior;
+import me.antileaf.signature.card.AbstractSignatureCard;
+import me.antileaf.signature.patches.SignaturePatch;
+import me.antileaf.signature.utils.SignatureHelper;
+
+import java.util.ArrayList;
+
+@SuppressWarnings("unused")
+public class SCVRenderCardPatch {
+	private static void renderHelper(SingleCardViewPopup scv, SpriteBatch sb,
+									 float x, float y, TextureAtlas.AtlasRegion img) {
+		ReflectionHacks.privateMethod(SingleCardViewPopup.class, "renderHelper",
+				SpriteBatch.class, float.class, float.class, TextureAtlas.AtlasRegion.class)
+				.invoke(scv, sb, x, y, img);
+	}
+
+	@SpirePatch(clz = SingleCardViewPopup.class, method = SpirePatch.CLASS)
+	public static class Fields {
+		public static SpireField<TextureAtlas.AtlasRegion> signature = new SpireField<>(() -> null);
+	}
+
+	@SpirePatch(clz = SingleCardViewPopup.class, method = "loadPortraitImg", paramtypez = {})
+	public static class LoadPortraitImgPatch {
+		@SpirePostfixPatch
+		public static void Postfix(SingleCardViewPopup _inst, AbstractCard ___card) {
+			if (SignatureHelper.hasSignature(___card)) {
+				String sig = ___card instanceof AbstractSignatureCard ?
+						((AbstractSignatureCard) ___card).getSignaturePortraitImgPath() :
+						SignatureHelper.getInfo(___card.cardID).portrait;
+
+				Fields.signature.set(_inst, SignatureHelper.load(sig));
+			}
+		}
+	}
+
+	@SpirePatch(clz = SingleCardViewPopup.class, method = "close", paramtypez = {})
+	public static class ClosePatch {
+		@SpirePostfixPatch
+		public static void Postfix(SingleCardViewPopup _inst) {
+			if (Fields.signature.get(_inst) != null)
+				Fields.signature.set(_inst, null);
+		}
+	}
+
+	@SpirePatch(clz = SingleCardViewPopup.class, method = "renderPortrait", paramtypez = {SpriteBatch.class})
+	public static class RenderPortraitPatch {
+		@SpirePrefixPatch
+		public static SpireReturn<Void> Prefix(SingleCardViewPopup _inst, SpriteBatch sb, AbstractCard ___card) {
+			if (___card.isLocked)
+				return SpireReturn.Continue();
+
+			if (SignatureHelper.shouldUseSignature(___card.cardID)) {
+				renderHelper(_inst, sb, Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F,
+						Fields.signature.get(_inst));
+
+				if (!SCVPanelPatch.Fields.hideDesc.get(_inst)) {
+					SignatureHelper.Style style = SignatureHelper.getStyle(___card);
+
+					TextureAtlas.AtlasRegion shadow = ___card.description.size() >= 4 || style.descShadowSmallP == null ?
+							style.descShadowP : style.descShadowSmallP;
+
+					if (shadow != null)
+						renderHelper(_inst, sb, Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F, shadow);
+				}
+
+				return SpireReturn.Return(null);
+			}
+
+			return SpireReturn.Continue();
+		}
+	}
+
+	private static TextureAtlas.AtlasRegion getFrameP(AbstractCard card) {
+		SignatureHelper.Style style = SignatureHelper.getStyle(card);
+
+		TextureAtlas.AtlasRegion frame;
+
+		if (card.type == AbstractCard.CardType.ATTACK) {
+			if (card.rarity == AbstractCard.CardRarity.RARE)
+				frame = style.cardTypeAttackRareP;
+			else if (card.rarity == AbstractCard.CardRarity.UNCOMMON)
+				frame = style.cardTypeAttackUncommonP;
+			else
+				frame = style.cardTypeAttackCommonP;
+		}
+		else if (card.type == AbstractCard.CardType.POWER) {
+			if (card.rarity == AbstractCard.CardRarity.RARE)
+				frame = style.cardTypePowerRareP;
+			else if (card.rarity == AbstractCard.CardRarity.UNCOMMON)
+				frame = style.cardTypePowerUncommonP;
+			else
+				frame = style.cardTypePowerCommonP;
+		}
+		else {
+			if (card.rarity == AbstractCard.CardRarity.RARE)
+				frame = style.cardTypeSkillRareP;
+			else if (card.rarity == AbstractCard.CardRarity.UNCOMMON)
+				frame = style.cardTypeSkillUncommonP;
+			else
+				frame = style.cardTypeSkillCommonP;
+		}
+
+		return frame;
+	}
+
+	@SpirePatch(clz = SingleCardViewPopup.class, method = "renderFrame", paramtypez = {SpriteBatch.class})
+	public static class RenderFramePatch {
+		@SpirePrefixPatch
+		public static SpireReturn<Void> Prefix(SingleCardViewPopup _inst, SpriteBatch sb, AbstractCard ___card) {
+			if (SignatureHelper.shouldUseSignature(___card.cardID)) {
+				TextureAtlas.AtlasRegion frame = getFrameP(___card);
+
+				if (frame != null)
+					renderHelper(_inst, sb, Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F, frame);
+
+				return SpireReturn.Return();
+			}
+
+			return SpireReturn.Continue();
+		}
+	}
+
+	@SpirePatch(clz = SingleCardViewPopup.class, method = "renderCardBanner", paramtypez = {SpriteBatch.class})
+	public static class RenderCardBannerPatch {
+		@SpirePrefixPatch
+		public static SpireReturn<Void> Prefix(SingleCardViewPopup _inst, SpriteBatch sb, AbstractCard ___card) {
+			if (SignatureHelper.shouldUseSignature(___card.cardID))
+				return SpireReturn.Return();
+
+			return SpireReturn.Continue();
+		}
+	}
+
+	@SpirePatch(clz = SingleCardViewPopup.class, method = "renderCardTypeText", paramtypez = {SpriteBatch.class})
+	public static class RenderCardTypeTextPatch {
+		private static class Locator extends SpireInsertLocator {
+			@Override
+			public int[] Locate(CtBehavior ctBehavior) throws CannotCompileException, PatchingException {
+				return LineFinder.findInOrder(ctBehavior,
+						new Matcher.MethodCallMatcher(FontHelper.class, "renderFontCentered"));
+			}
+		}
+
+		@SpireInsertPatch(locator = Locator.class, localvars = {"label"})
+		public static SpireReturn<Void> Insert(SingleCardViewPopup _inst, SpriteBatch sb,
+											   AbstractCard ___card, String label) {
+			if (SignatureHelper.shouldUseSignature(___card.cardID)) {
+				if (getFrameP(___card) != null) {
+					Color cardTypeColor = ReflectionHacks.getPrivateStatic(SingleCardViewPopup.class,
+							"CARD_TYPE_COLOR");
+
+					FontHelper.renderFontCentered(sb, FontHelper.panelNameFont, label,
+							(float) Settings.WIDTH / 2.0F,
+							(float) Settings.HEIGHT / 2.0F - 392.0F * Settings.scale,
+							cardTypeColor);
+				}
+
+				return SpireReturn.Return();
+			}
+
+			return SpireReturn.Continue();
+		}
+	}
+
+	@SpirePatch(clz = SingleCardViewPopup.class, method = "renderDescription", paramtypez = {SpriteBatch.class})
+	public static class RenderDescriptionPatch {
+		@SpirePrefixPatch
+		public static SpireReturn<Void> Prefix(SingleCardViewPopup _inst, SpriteBatch sb, AbstractCard ___card) {
+			if (SignatureHelper.shouldUseSignature(___card.cardID) &&
+					SCVPanelPatch.Fields.hideDesc.get(_inst))
+				return SpireReturn.Return();
+
+			return SpireReturn.Continue();
+		}
+	}
+
+	@SpirePatch(clz = SingleCardViewPopup.class, method = "renderDescriptionCN", paramtypez = {SpriteBatch.class})
+	public static class RenderDescriptionCNPatch {
+		@SpirePrefixPatch
+		public static SpireReturn<Void> Prefix(SingleCardViewPopup _inst, SpriteBatch sb, AbstractCard ___card) {
+			if (SignatureHelper.shouldUseSignature(___card.cardID) &&
+					SCVPanelPatch.Fields.hideDesc.get(_inst))
+				return SpireReturn.Return();
+
+			return SpireReturn.Continue();
+		}
+	}
+
+	@SpirePatch(clz = SingleCardViewPopup.class, method = "renderTips", paramtypez = {SpriteBatch.class})
+	public static class RenderTipsPatch {
+		private static class Locator extends SpireInsertLocator {
+			@Override
+			public int[] Locate(CtBehavior ctBehavior) throws CannotCompileException, PatchingException {
+				return LineFinder.findInOrder(ctBehavior,
+						new Matcher.MethodCallMatcher(AbstractCard.class,
+								"renderCardPreviewInSingleView"));
+			}
+		}
+
+		@SpireInsertPatch(locator = Locator.class)
+		public static void Insert(SingleCardViewPopup _inst, SpriteBatch sb, AbstractCard ___card) {
+			if (SignatureHelper.shouldUseSignature(___card.cardsToPreview.cardID))
+				SignaturePatch.setPreviewTransparency(___card.cardsToPreview,
+						SCVPanelPatch.Fields.hideDesc.get(_inst) ? 0.0F : 1.0F);
+		}
+	}
+
+	@SpirePatch(clz = MultiCardPreviewRenderer.RenderMultiCardPreviewInSingleViewPatch.class,
+			method = "Postfix", paramtypez = {SingleCardViewPopup.class, SpriteBatch.class})
+	public static class BaseModMultiCardPreviewSCVPatch {
+		@SpirePrefixPatch
+		public static void Prefix(SingleCardViewPopup _inst, SpriteBatch sb) {
+			AbstractCard card = ReflectionHacks.getPrivate(_inst, SingleCardViewPopup.class, "card");
+
+			if (MultiCardPreview.multiCardPreview.get(card) != null)
+				for (AbstractCard preview : MultiCardPreview.multiCardPreview.get(card))
+					if (SignatureHelper.shouldUseSignature(preview.cardID))
+						SignaturePatch.setPreviewTransparency(preview,
+								SCVPanelPatch.Fields.hideDesc.get(_inst) ? 0.0F : 1.0F);
+		}
+	}
+
+	@SpirePatch(clz = MultiCardPreviewRenderer.RenderMultiCardPreviewInSingleViewPatch.class,
+			method = "Postfix", paramtypez = {SingleCardViewPopup.class, SpriteBatch.class})
+	public static class AvoidOverlappingWithPanelPatch {
+		@SpireInsertPatch(rloc = 25, localvars = {"horizontalOnly", "verticalNext", "position", "offset", "toPreview"})
+		public static void Insert(SingleCardViewPopup _inst, SpriteBatch sb, AbstractCard ___card,
+								  boolean horizontalOnly, @ByRef boolean[] verticalNext,
+								  Vector2 position, Vector2 offset, AbstractCard toPreview) {
+			if (SignatureHelper.hasSignature(___card) && SignatureHelper.isUnlocked(___card.cardID)) {
+				if (___card instanceof AbstractSignatureCard && ((AbstractSignatureCard) ___card).dontAvoidSCVPanel)
+					return;
+
+				if (SignatureHelper.getInfo(___card.cardID).dontAvoidSCVPanel)
+					return;
+
+				ArrayList<AbstractCard> previews = MultiCardPreview.multiCardPreview.get(___card);
+
+				if (previews.size() > 1 && toPreview == previews.get(1)) {
+					ReflectionHacks.privateMethod(MultiCardPreviewRenderer.class, "reposition",
+							Vector2.class, Vector2.class, boolean.class)
+							.invoke(null, position, offset, horizontalOnly || !verticalNext[0]);
+
+					verticalNext[0] = !verticalNext[0];
+				}
+			}
+		}
+	}
+}
---
+++ src/main/java/me/antileaf/signature/utils/SignatureHelper.java
@@ -0,0 +1,268 @@
+package me.antileaf.signature.utils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import me.antileaf.signature.card.AbstractSignatureCard;
+import me.antileaf.signature.patches.SignaturePatch;
+import me.antileaf.signature.patches.card.UnlockConditionPatch;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class SignatureHelper {
+	private static final Logger logger = LogManager.getLogger(SignatureHelper.class);
+
+	public static final float FADE_DURATION = 0.3F;
+	public static final float FORCED_FADE_DURATION = 0.5F;
+
+	public static final Style DEFAULT_STYLE;
+
+	private static final Map<String, Info> registered = new HashMap<>();
+
+	private static final Map<String, Boolean> unlocked = new HashMap<>();
+	private static final Map<String, Boolean> enabled = new HashMap<>();
+	private static final Map<String, Texture> cache = new HashMap<>();
+
+	private static final Set<String> noDebugging = new HashSet<>();
+	private static final Set<String> noDebuggingPrefixes = new HashSet<>();
+
+	public static TextureAtlas.AtlasRegion load(String path) {
+		Texture t;
+
+		if (cache.containsKey(path))
+			t = cache.get(path);
+		else {
+			if (Gdx.files.internal(path).exists())
+				t = ImageMaster.loadImage(path);
+			else
+				t = null;
+
+			cache.put(path, t);
+		}
+
+		return t == null ? null :
+				new TextureAtlas.AtlasRegion(t, 0, 0, t.getWidth(), t.getHeight());
+	}
+
+	public static void register(String id, Info info) {
+		AbstractCard card = CardLibrary.getCard(id);
+
+		if (card == null) {
+			logger.error("Card with ID {} not found", id);
+			return;
+		}
+
+		if (card instanceof AbstractSignatureCard) {
+			logger.warn("Please do not register a card that already extends AbstractSignatureCard");
+			return;
+		}
+
+		registered.put(id, info);
+	}
+
+	public static boolean isRegistered(String id) {
+		return registered.containsKey(id);
+	}
+
+	public static Info getInfo(String id) {
+		return registered.get(id);
+	}
+
+	public static Style getStyle(AbstractCard card) {
+		return card instanceof AbstractSignatureCard ?
+				((AbstractSignatureCard) card).style :
+				getInfo(card.cardID).style;
+	}
+
+	public static boolean hasSignature(AbstractCard card) {
+		if (card instanceof AbstractSignatureCard)
+			return ((AbstractSignatureCard) card).hasSignature;
+		else
+			return isRegistered(card.cardID);
+	}
+
+	public static boolean isUnlocked(String id) {
+		if (ConfigHelper.enableDebugging() && !noDebugging.contains(id) &&
+				noDebuggingPrefixes.stream().noneMatch(id::startsWith))
+			return true;
+
+		if (!unlocked.containsKey(id))
+			unlocked.put(id, ConfigHelper.isSignatureUnlocked(id));
+
+		return unlocked.get(id);
+	}
+
+	public static void setDebugging(String id, boolean available) {
+		if (available)
+			noDebugging.add(id);
+		else
+			noDebugging.remove(id);
+	}
+
+	public static void noDebugging(String id) {
+		noDebugging.add(id);
+	}
+
+	public static void noDebuggingPrefix(String prefix) {
+		noDebuggingPrefixes.add(prefix);
+	}
+
+	public static void unlock(String id, boolean unlock) {
+		ConfigHelper.setSignatureUnlocked(id, unlock);
+		unlocked.put(id, unlock);
+	}
+
+	public static String getUnlockCondition(String id) {
+		return UnlockConditionPatch.Fields.unlockCondition.get(CardCrawlGame.languagePack.getCardStrings(id));
+	}
+
+	public static void addUnlockConditions(String jsonFile) {
+		Map<String, String> conditions = (new Gson()).fromJson(
+				Gdx.files.internal(jsonFile).readString(String.valueOf(StandardCharsets.UTF_8)),
+				(new TypeToken<Map<String, String>>() {}).getType());
+
+		for (Map.Entry<String, String> entry : conditions.entrySet())
+			UnlockConditionPatch.Fields.unlockCondition.set(
+					CardCrawlGame.languagePack.getCardStrings(entry.getKey()),
+					entry.getValue());
+	}
+
+	public static boolean isEnabled(String id) {
+		if (!enabled.containsKey(id))
+			enabled.put(id, ConfigHelper.isSignatureEnabled(id));
+
+		if (!isUnlocked(id))
+			enable(id, false);
+
+		return enabled.get(id);
+	}
+
+	public static void enable(String id, boolean enable) {
+		ConfigHelper.setSignatureEnabled(id, enable);
+		enabled.put(id, enable);
+	}
+
+	public static boolean shouldUseSignature(String id) {
+		return isUnlocked(id) && isEnabled(id);
+	}
+
+	public static class Info {
+		public final String img, portrait;
+		public final boolean dontAvoidSCVPanel;
+		public final Style style;
+
+		public Info(String img, String portrait, boolean dontAvoidSCVPanel, Style style) {
+			this.img = img;
+			this.portrait = portrait;
+			this.dontAvoidSCVPanel = dontAvoidSCVPanel;
+			this.style = style;
+		}
+
+		public Info(String img, String portrait, boolean dontAvoidSCVPanel) {
+			this(img, portrait, dontAvoidSCVPanel, DEFAULT_STYLE);
+		}
+
+		public Info(String img, String portrait, Style style) {
+			this(img, portrait, false, style);
+		}
+
+		public Info(String img, String portrait) {
+			this(img, portrait, false);
+		}
+	}
+
+	public static class Style {
+		public TextureAtlas.AtlasRegion cardTypeAttackCommon, cardTypeAttackUncommon, cardTypeAttackRare;
+		public TextureAtlas.AtlasRegion cardTypeSkillCommon, cardTypeSkillUncommon, cardTypeSkillRare;
+		public TextureAtlas.AtlasRegion cardTypePowerCommon, cardTypePowerUncommon, cardTypePowerRare;
+		public TextureAtlas.AtlasRegion cardTypeAttackCommonP, cardTypeAttackUncommonP, cardTypeAttackRareP;
+		public TextureAtlas.AtlasRegion cardTypeSkillCommonP, cardTypeSkillUncommonP, cardTypeSkillRareP;
+		public TextureAtlas.AtlasRegion cardTypePowerCommonP, cardTypePowerUncommonP, cardTypePowerRareP;
+		public TextureAtlas.AtlasRegion descShadow, descShadowP;
+		public TextureAtlas.AtlasRegion descShadowSmall, descShadowSmallP;
+
+		public Style() {}
+
+		public Style(String cardTypeAttackCommon, String cardTypeAttackUncommon, String cardTypeAttackRare,
+					 String cardTypeSkillCommon, String cardTypeSkillUncommon, String cardTypeSkillRare,
+					 String cardTypePowerCommon, String cardTypePowerUncommon, String cardTypePowerRare,
+					 String cardTypeAttackCommonP, String cardTypeAttackUncommonP, String cardTypeAttackRareP,
+					 String cardTypeSkillCommonP, String cardTypeSkillUncommonP, String cardTypeSkillRareP,
+					 String cardTypePowerCommonP, String cardTypePowerUncommonP, String cardTypePowerRareP,
+					 String descShadow, String descShadowP,
+					 String descShadowSmall, String descShadowSmallP) {
+			this.cardTypeAttackCommon = load(cardTypeAttackCommon);
+			this.cardTypeAttackUncommon = load(cardTypeAttackUncommon);
+			this.cardTypeAttackRare = load(cardTypeAttackRare);
+			this.cardTypeSkillCommon = load(cardTypeSkillCommon);
+			this.cardTypeSkillUncommon = load(cardTypeSkillUncommon);
+			this.cardTypeSkillRare = load(cardTypeSkillRare);
+			this.cardTypePowerCommon = load(cardTypePowerCommon);
+			this.cardTypePowerUncommon = load(cardTypePowerUncommon);
+			this.cardTypePowerRare = load(cardTypePowerRare);
+			this.cardTypeAttackCommonP = load(cardTypeAttackCommonP);
+			this.cardTypeAttackUncommonP = load(cardTypeAttackUncommonP);
+			this.cardTypeAttackRareP = load(cardTypeAttackRareP);
+			this.cardTypeSkillCommonP = load(cardTypeSkillCommonP);
+			this.cardTypeSkillUncommonP = load(cardTypeSkillUncommonP);
+			this.cardTypeSkillRareP = load(cardTypeSkillRareP);
+			this.cardTypePowerCommonP = load(cardTypePowerCommonP);
+			this.cardTypePowerUncommonP = load(cardTypePowerUncommonP);
+			this.cardTypePowerRareP = load(cardTypePowerRareP);
+			this.descShadow = load(descShadow);
+			this.descShadowP = load(descShadowP);
+			this.descShadowSmall = load(descShadowSmall);
+			this.descShadowSmallP = load(descShadowSmallP);
+		}
+	}
+
+	static {
+		 DEFAULT_STYLE = new Style(
+				"SignatureLib/512/attack_common.png",
+				"SignatureLib/512/attack_uncommon.png",
+				"SignatureLib/512/attack_rare.png",
+				"SignatureLib/512/skill_common.png",
+				"SignatureLib/512/skill_uncommon.png",
+				"SignatureLib/512/skill_rare.png",
+				"SignatureLib/512/power_common.png",
+				"SignatureLib/512/power_uncommon.png",
+				"SignatureLib/512/power_rare.png",
+				"SignatureLib/1024/attack_common.png",
+				"SignatureLib/1024/attack_uncommon.png",
+				"SignatureLib/1024/attack_rare.png",
+				"SignatureLib/1024/skill_common.png",
+				"SignatureLib/1024/skill_uncommon.png",
+				"SignatureLib/1024/skill_rare.png",
+				"SignatureLib/1024/power_common.png",
+				"SignatureLib/1024/power_uncommon.png",
+				"SignatureLib/1024/power_rare.png",
+				"SignatureLib/512/desc_shadow.png",
+				"SignatureLib/1024/desc_shadow.png",
+				"SignatureLib/512/desc_shadow_small.png",
+				"SignatureLib/1024/desc_shadow_small.png"
+		);
+	}
+
+	public static boolean usePatch(String id) {
+		return SignatureHelper.isRegistered(id) && SignatureHelper.shouldUseSignature(id);
+	}
+
+	public static void forceToShowDescription(AbstractCard card) {
+		if (card instanceof AbstractSignatureCard)
+			((AbstractSignatureCard) card).forceToShowDescription();
+		else
+			SignaturePatch.forceToShowDescription(card);
+	}
+}
